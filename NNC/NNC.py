51bb9241411a23630e17f1ce68c3c6b390b02991
porat-ah <porat2001@gmail.com>
2022-05-09 17:28:02 +0300

    added parallel calculation

--- NNC/NNC.py
+++ NNC/NNC.py
@@ -6,7 +6,8 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 
 class NNC:
-    def __init__(self, algorithm="brute", metric="minkowski", p= 2, n_jobs=1):
+    EPS = 0.0001
+    def __init__(self, algorithm="brute", metric="minkowski", p=2, n_jobs=1):
         """
         :param algorithm:{"brute" , "prune"} Algorithm used to compute the cardinality subset.
         :param metric:{"euclidean", "manhattan", "minkowski", "chebyshev"} or a callable function. Distance metric to use for finding gamma.
@@ -18,11 +19,11 @@ class NNC:
         self.S_gamma = list()
         self.algorithm = algorithm
         if callable(metric):
-            self.dist = metric
+            self.dist = (True, metric, metric)
+        elif metric == "minkowski":
+            self.dist = (True, Metric(metric, p), metric)
         else:
-            self.dist = Metric(metric, p)
-        self.x1 = None
-        self.x2 = None
+            self.dist = (False, Metric(metric, p), metric)
         self.scale = None
         self.n_jobs = n_jobs
 
@@ -34,15 +35,14 @@ class NNC:
         :param y: numpy array , Target values
         :return: self
         """
-        X = minmax_scale(X, feature_range=(0,1))
-        self.scale = self.dist(np.ones_like(X[0]), np.zeros_like(X[0]))
+        X = minmax_scale(X, feature_range=(0, 1))
+        self.scale = self.dist[1](np.ones_like(X[0]), np.zeros_like(X[0]))
         groups = []
         for label in np.unique(y):
             groups.append(X[y == label])
         for i in range(len(groups)):
             for j in range(i + 1, len(groups)):
-                i_j_distances = np.min(
-                    pairwise_distances(groups[i], groups[j], metric=self.dist, n_jobs=self.n_jobs) / self.scale)
+                i_j_distances = self.min_pairwise_distance(groups[i], groups[j])
                 if self.gamma > i_j_distances:
                     self.gamma = i_j_distances
         return self
@@ -58,11 +58,11 @@ class NNC:
         _X = minmax_scale(X, feature_range=(0, 1))
         X_new = self.brute(_X)
         mask = np.array(self.find_common_arrays_location(_X, X_new))
-        y_new = np.zeros_like(X_new[:, 0])
-        y_new[mask[:, 1]] = y[mask[:, 0]]
         if self.algorithm == "prune":
+            y_new = np.zeros_like(X_new[:, 0])
+            y_new[mask[:, 1]] = y[mask[:, 0]]
             X_new = self.prune(X_new, y_new)
-        mask = np.array(self.find_common_arrays_location(_X, X_new))
+            mask = np.array(self.find_common_arrays_location(_X, X_new))
         return X[mask[:, 0]], y[mask[:, 0]]
 
     def fit_transform(self, X: np.ndarray, y: np.ndarray):
@@ -79,12 +79,7 @@ class NNC:
     def brute(self, X: np.ndarray, y=None):
         self.S_gamma.append(X[0])
         for p in X:
-            b = True
-            for q in self.S_gamma:
-                b &= (self.dist(p, q)/self.scale >= self.gamma)
-                if not b:
-                    break
-            if b:
+            if self.min_pairwise_distance([p], self.S_gamma) >= self.gamma - NNC.EPS:
                 self.S_gamma.append(p)
         return np.unique(np.array(self.S_gamma), axis=0)
 
@@ -95,16 +90,15 @@ class NNC:
             for j, p in enumerate(X):
                 b = True
                 for q in X[y != y[j]]:
-                    b &= self.dist(p, q)/self.scale >= np.power(2., i + 1)
+                    b &= self.dist[1](p, q) / self.scale >= np.power(2., i + 1) - NNC.EPS
                     if not b:
                         break
                 if b:
                     for p_ in X:
-                        if (np.any(p != p_)) and (self.dist(p, p_)/self.scale < np.power(2.0, i) - self.gamma):
+                        if (np.any(p != p_)) and (self.dist[1](p, p_) / self.scale < np.power(2.0, i) - self.gamma):
                             np.delete(S_gama, np.where(np.atleast_1d(np.all(S_gama == p_)).nonzero()))
 
         return S_gama
-
 
     def find_common_arrays_location(self, arr1, arr2):
         index = []
@@ -115,7 +109,14 @@ class NNC:
                     break
         return index
 
+    def min_pairwise_distance(self, g1, g2):
+        if self.dist[0]:
+            return np.min(
+                pairwise_distances(g1, g2, metric=self.dist[1], n_jobs=self.n_jobs) / self.scale)
+        else:
+            return np.min(
+                pairwise_distances(g1, g2, metric=self.dist[2], n_jobs=self.n_jobs) / self.scale)
+
     def __str__(self):
-        return "NNC(algorithm='{alg}', metric='{mtr}', p={p})".format(alg=self.algorithm,
-                                                                     mtr=self.dist.metric,
-                                                                     p=self.dist.p)
+        return "NNC(algorithm='{alg}', metric='{mtr}')".format(alg=self.algorithm,
+                                                               mtr=self.dist[2])
